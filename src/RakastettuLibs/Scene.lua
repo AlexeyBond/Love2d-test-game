791d29c327d02f4456022bf6ca50385ec126a433
Dmitry Shatov <d.shatty@gmail.com>
2014-04-23 19:30:11 +0700

    Каркас класса сцены

---
+++ src/RakastettuLibs/Scene.lua
@@ -0,0 +1,23 @@
+local Scene = {}
+
+function Scene.new()
+	print("Scene.new() called")
+
+	local scene = {}
+	
+	scene._layers = {}
+
+	scene._camera = {}
+	local cam = scene._camera
+	cam._originPt = {x = 0, y = 0}
+	cam._sizes = {width = 0, height = 0}
+	cam._angle = 0
+
+	function scene:addLayer(layer)
+		print("scene:addLayer(layer) called")
+	end
+
+	return scene
+end
+
+return Scene
--- src/main.lua
+++ src/main.lua
@@ -1,20 +1,27 @@
+rkstlib = {}
+rkstlib.scene = require "RakastettuLibs.Scene"
+game = {}
+
 function love.load()
-	local width = 128
-	local height = 128
+	local width = 640
+	local height = 480
 
 	if love.window ~= nil then
 		love.window.setMode(width, height)
 	else
 		love.graphics.setMode(width, height)
 	end
+
+	game.scene = rkstlib.scene.new()
+	game.scene:addLayer(nil)
 end
 
 
 function love.draw()
-
+	--game.scene:draw()
 end
 
 
 function love.update(dt)
-
+	--game.scene:update(dt)
 end
