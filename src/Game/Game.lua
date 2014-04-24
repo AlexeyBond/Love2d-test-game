b9849b4fe0a58c2d293d4b8f2de476c5645e4ba0
Dmitriy Shatov <d.shatty@gmail.com>
2014-04-24 14:53:28 +0700

    Вынес инициализацию игры со сценой в отдельный модуль, камера теперь ловит событие window resize

---
+++ src/Game/Game.lua
@@ -0,0 +1,58 @@
+rkstlib.scene = require "RakastettuLibs.Scene"
+rkstlib.layer = require "RakastettuLibs.Layer"
+rkstlib.node = require "RakastettuLibs.Node"
+
+local Game = {} --game with one scene
+
+function Game:resizeWindow(width, height)
+	local cam = self.scene._camera
+	cam._sizes.width = width
+	cam._sizes.height = height
+
+	--send event to layers?
+end
+
+function Game:init(width, height)
+	print("Game:init() called")
+
+	self.scene = rkstlib.scene.new()
+
+	self:resizeWindow(width, height)
+	self:_initTestScene()
+end
+
+--[[
+Тестовая сцена
+Один слой с двумя нодами
+Вращение и зум камеры
+]]--
+function Game:_initTestScene()
+	--test----------------
+	local mainLayer = rkstlib.layer.new()
+
+	local node1 = rkstlib.node.new()
+	node1:setRect(50, 50, 30, 40)
+	node1:moveTo(0, 0)
+	mainLayer:addNode(node1)
+
+	local node2 = rkstlib.node.new()
+	node2:setRect(10, 20, 30, 40)
+	node2:moveTo(100, -150)
+	mainLayer:addNode(node2)
+
+	self.scene:addLayer(mainLayer)
+
+	function game.scene:update(dt)
+		local phi = 2
+		local zv = 1
+		self._camera._angle = (self._camera._angle + dt * phi) % (2 * math.pi)
+		self._camera._zoom = self._camera._zoom + zv * dt
+		if self._camera._zoom > 2 then
+			self._camera._zoom = 0.5
+			print("over")
+		end
+	end
+	----------------------
+end
+
+return Game
--- src/main.lua
+++ src/main.lua
@@ -3,55 +3,26 @@ rkstlib.scene = require "RakastettuLibs.Scene"
 rkstlib.layer = require "RakastettuLibs.Layer"
 rkstlib.node = require "RakastettuLibs.Node"
 
-game = {}
+game = require "Game.Game"
 
 function love.load()
 	local width = 640
 	local height = 480
 
-	if love.window ~= nil then
-		love.window.setMode(width, height)
-	else
-		love.graphics.setMode(width, height)
-	end
+	if love.window ~= nil then -- v.0.9.0
+		love.window.setMode(width, height, {resizable = true,
+				vsync = true, minwidth = 320, minheight = 240})
 
-	game.scene = rkstlib.scene.new()
-	do
-		local cam = game.scene._camera
-		cam._sizes.width = width
-		cam._sizes.height = height
-	end
-
-	initTestScene()
-end
-
-function initTestScene()
-	--test----------------
-	local mainLayer = rkstlib.layer.new()
-
-	local node1 = rkstlib.node.new()
-	node1:setRect(50, 50, 30, 40)
-	node1:moveTo(0, 0)
-	mainLayer:addNode(node1)
-
-	local node2 = rkstlib.node.new()
-	node2:setRect(10, 20, 30, 40)
-	node2:moveTo(100, -150)
-	mainLayer:addNode(node2)
-
-	game.scene:addLayer(mainLayer)
-
-	function game.scene:update(dt)
-		local phi = 2
-		local zv = 1
-		self._camera._angle = (self._camera._angle + dt * phi) % (2 * math.pi)
-		self._camera._zoom = self._camera._zoom + zv * dt
-		if self._camera._zoom > 2 then
-			self._camera._zoom = 0.5
-			print("over")
+		--register resize callback
+		function love.resize(w, h)
+			game:resizeWindow(w, h)
 		end
+
+	else  -- v.0.8.0
+		love.graphics.setMode(width, height, false, true)
 	end
-	----------------------
+
+	game:init(width, height)
 end
 
 function love.draw()
